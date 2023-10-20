dd0783f2b42ac4f295d9d8922c38bb4858ef74fd
AnushaHk24 <hkanusha.1999@gmail.com>
2023-10-20 12:09:09 +0530

    Controller class Commit

---
+++ src/main/java/com/cg/controller/LibraryController.java
@@ -0,0 +1,38 @@
+package com.cg.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.entity.Library;
+import com.cg.service.LibraryService;
+
+
+@RestController
+@RequestMapping("/library")
+public class LibraryController  {
+	private LibraryService service;
+	
+	@PostMapping("/")
+
+	public String addProduct(@RequestBody Library l){
+
+		 return service.addLibrary(l);
+
+	}
+
+	@GetMapping("/getAll")
+
+	public List<Library> getAll(){
+
+		return service.getLibraryList();
+
+	}	
+
+}
+
