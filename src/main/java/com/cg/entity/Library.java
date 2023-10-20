43d379ba160c70aefb8bf190893d1a14a9b82bf4
AnushaHk24 <hkanusha.1999@gmail.com>
2023-10-20 11:49:03 +0530

    Entity class committed

--- src/main/java/com/cg/entity/Library.java
+++ src/main/java/com/cg/entity/Library.java
@@ -1,4 +1,56 @@
 package com.cg.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Library {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	private String location;
+	
+	private Library() {}
+
+	public Library(int id, String name, String location) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.location = location;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "Library [id=" + id + ", name=" + name + ", location=" + location + "]";
+	}
+	
+	
 }
